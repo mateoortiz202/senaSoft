31d9009b124f4dfdf5123c2ed155b3775af729e9
mateo <mateoortiz202@gmail.com>
2022-09-07 12:00:15 -0500

    login y nav

--- sondeo/urls.py
+++ sondeo/urls.py
@@ -11,4 +11,16 @@ urlpatterns = [
 
     path('login/', views.login, name="login"),
     path('logear=', views.logear, name="logear"),
+    
+    path('cerrarSesion/', views.cerrarSesion, name="cerrarSesion"),
+    
+    
+    path('urlAdmin/', views.urlAdmin, name="urlAdmin"),
+    path('logAdmin/', views.logAdmin, name="logAdmin"),
+    path('formSondeo/', views.formSondeo, name="formSondeo"),
+    
+    
+
+    
+    
 ]
